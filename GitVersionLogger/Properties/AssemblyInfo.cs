0ca90a7a3e3583c4b41c7f225bbb3fbfd3254c86
Pascal Paradis <pascal.paradis@gmail.com>
2013-05-13 06:27:12 -0400

    Préparation du projet démo

--- GitVersionLogger/Properties/AssemblyInfo.cs
+++ GitVersionLogger/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8c3a3473-c2bd-4bfe-94ed-e21e01a44247")]
+[assembly: Guid("3f2be6d0-e6bc-486a-a3b2-321f32ea3064")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
