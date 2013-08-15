8983662db38204d0b7f6bc59800000d7dfd41500
Pascal Paradis <pascal.paradis@gmail.com>
2013-08-14 20:44:57 -0400

    Mise à jour NuGet

--- FrenchCoding.RedirectExperiments/Scripts/_references.js
+++ FrenchCoding.RedirectExperiments/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-2.0.2.js" />
+﻿/// <reference path="jquery-2.0.3.js" />
 /// <reference path="modernizr-2.6.2.js" />
 /// <reference path="jquery-ui-1.10.3.js" />
 /// <reference path="modernizr-2.0.6-development-only.js" />
