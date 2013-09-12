53c759d4979431273e432989a92dcadd62345eba
Pascal Paradis <pascal.paradis@gmail.com>
2013-09-11 20:27:42 -0400

    Mise à jour du script

--- GitVersionLogger/Build/git-version-extract.ps1
+++ GitVersionLogger/Build/git-version-extract.ps1
@@ -4,9 +4,9 @@ Param(
 $time = Get-Date
 $data = $time.ToShortDateString() + " " + $time.ToLongTimeString() + " - " + (git rev-parse --abbrev-ref HEAD) + " - " + (git describe --dirty --always)
 
-$data | Out-File $filePath + "\version.txt"
+$data | Out-File "$filePath\version.txt"
 
-$original_file = $filePath + "\Build\AssemblyInfo.template.cs"
+$original_file = $filePath + "/\Build\AssemblyInfo.template.cs"
 $destination_file =  $filePath + "\Properties\AssemblyInfo.cs"
 
 (Get-Content $original_file) -replace "##GIT-VERSION-INFO##", $data | Set-Content $destination_file
